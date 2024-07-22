acc3311eeaffe67fa0ee2b3a4dc708c160d04987
Daniel <danielyoussef1@yahoo.com>
2024-07-22 01:45:21 -0700

    fixed cell width to fit to container

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const container = document.querySelector("#container");
 
 function createGrid(gridSize) {
+  let resizedCell = 500 / gridSize;
+
   for (i = 1; i <= gridSize; i++) {
     let row = container.appendChild(document.createElement("div"));
     for (j = 1; j <= gridSize; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
-      let resizedCell = 500 / gridSize;
+
       cell.style.width = parseFloat(resizedCell).toFixed(1) + "px";
       cell.style.height = parseFloat(resizedCell).toFixed(1) + "px";
+
       row.appendChild(cell);
     }
   }
 }
 
-createGrid(2);
+createGrid(16);
